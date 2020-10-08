f9ab50c703b65857c7f6927c63260ecd0543fea2
Justin Beaurone <19704046+jmb-42@users.noreply.github.com>
2020-10-08 12:00:27 -0700

    init protocol

---
+++ composable-protocol/README.md
@@ -0,0 +1,16 @@
+# composable
+
+## Install
+ - Download & install
+ - ```bash
+   git clone
+   https://github.com/blockchainatucla/ethonline
+   cd ethonline
+   npm install
+   ```
+ - Save your testnet mnemonic and infura key into `.env`:
+ - ```bash
+   # contents of .env
+   INFURA_KEY="abcdefghijklmnop"
+   MNEMONIC="this is not actually a valid mnemonic but it should make sense"
+   ```
---
+++ composable-protocol/truffle-config.js
@@ -0,0 +1,53 @@
+/**
+ *
+ * More information about truffle configuration can be found at:
+ *
+ * truffleframework.com/docs/advanced/configuration
+ *
+ *
+ * Infura accounts are available for free at: infura.io/register.
+ *
+ *
+ * Save a mnemonic and an infura key into `.env`
+ *
+ */
+
+require('dotenv').config()
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+// provider URLs for different test nets
+const GANACHE_URL = "http://localhost:8545"
+const ROPSTEN_URL = "https://ropsten.infura.io/v3/" + process.env.INFURA_KEY;
+const KOVAN_URL = "https://kovan.infura.io/v3/" + process.env.INFURA_KEY;
+
+module.exports = {
+  networks: {
+    development: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, GANACHE_URL),
+      network_id: "*",       // Any network (default: none)
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, ROPSTEN_URL),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+    kovan: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, KOVAN_URL),
+      network_id: 42,       // Kovan's id
+      skipDryRun: true
+    }
+  },
+  compilers: {
+    solc: {
+      // version: "0.5.1",
+      // settings: {
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    },
+  },
+};
