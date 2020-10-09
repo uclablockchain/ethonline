88364a84333f24d93613546e2d141046f2c3ffd2
Justin Beaurone <19704046+jmb-42@users.noreply.github.com>
2020-10-08 19:50:11 -0700

    connect pocketnetwork

---
+++ composable-protocol/ganache-fork-eth-archival.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+# load environment variables from .env file
+source .env
+
+# open ganache on 127.0.0.1:8545
+ganache-cli -h "127.0.0.1" -p 8545 -f $ARCHIVAL_URL$APPLICATION_ID -m $MNEMONIC
---
+++ composable-protocol/scripts/getBalance.js
@@ -0,0 +1,11 @@
+
+module.exports = async (callback) => {
+  /**
+  * @notice The point of this script is to get the Ether balance of whatever
+  *         address is saved here, e.g., Yearn: Deployer
+  *
+  **/
+  const YearnDeployerAddress = "0x2D407dDb06311396fE14D4b49da5F0471447d45C";
+  let balance = await web3.eth.getBalance(YearnDeployerAddress);
+  callback(balance);
+};
