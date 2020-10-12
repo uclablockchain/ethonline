369e2211674dcd67fe1e68e64dfc95210b701cff
technicallyty <48813565+technicallyty@users.noreply.github.com>
2020-10-12 16:24:20 -0700

    Add files via upload

---
+++ composable-ui/src/components/Logo/Logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Logo.css';
+
+const Logo = () => {
+    return (
+    <div className="logo-container">
+        <div className="inside">Composable</div>
+        <div className="grad-left"></div>
+        <div className="grad-right"></div>
+    </div>
+    )
+}
+
+export default Logo
---
+++ composable-ui/src/icons/icons.js
@@ -0,0 +1,12 @@
+import React from 'react'
+ export default function SettingsCog() {
+    return (
+        <>
+        <svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" dataReactroot="">
+       <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="1" stroke="#e1d3e1" fill="#6e6e8c" d="M19 22H5C3.34 22 2 20.66 2 19V5C2 3.34 3.34 2 5 2H19C20.66 2 22 3.34 22 5V19C22 20.66 20.66 22 19 22Z"></path>
+       <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="1" stroke="#e1d3e1" d="M17 7L7 17"></path>
+       <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="1" stroke="#e1d3e1" d="M17 12V7H12"></path>
+       <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="1" stroke="#e1d3e1" d="M7 12V17H12"></path>
+       </svg>
+       </>
+       )}
